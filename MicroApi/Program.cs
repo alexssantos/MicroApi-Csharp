f0caa59cf0ece2ebfc378968d2463686c1e985f5
Alex Santos <arkanjo.alex@gmail.com>
2021-02-06 18:46:35 -0300

    Added 'micro' Service  by dependency injection

--- MicroApi/Program.cs
+++ MicroApi/Program.cs
@@ -1,7 +1,10 @@
+using MicroApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text.Json;
 
 namespace MicroApi
 {
@@ -18,15 +21,38 @@ namespace MicroApi
 
     public class Startup
     {
-        public void Configure(IApplicationBuilder appBuilder, IWebHostEnvironment env)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<CountryService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder appBuilder)
+        {
+
             appBuilder
+                .UsePathBase(new PathString("/api"))
                 .UseRouting()
                 .UseEndpoints(endpoints =>
                 {
-                    endpoints.MapGet("api/", async (context) =>
+                    endpoints.MapGet("/countries/{id:int}", async (context) =>
                     {
-                        await context.Response.WriteAsync("Hello World!");
+                        var countryService = context.Request.HttpContext.RequestServices.GetRequiredService<CountryService>();
+                        var id = int.Parse((string)context.Request.RouteValues["id"]);
+                        var country = await countryService.Get(id);
+                        var response = country != null ? JsonSerializer.Serialize(country) : "country not found!";
+
+                        await context.Response.WriteAsync(response);
+                    });
+
+                    endpoints.MapGet("/countries", async (context) =>
+                    {
+                        var countryService = context.Request.HttpContext.RequestServices.GetRequiredService<CountryService>();
+                        var countryList = await countryService.Get();
+                        var response = JsonSerializer.Serialize(countryList);
+
+                        await context.Response.WriteAsync(response);
                     });
                 });
         }
---
+++ MicroApi/Services/CountryService.cs
@@ -0,0 +1,43 @@
+﻿using MicroApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroApi.Services
+{
+    public class CountryService
+    {
+        private readonly List<Country> _countries;
+        public CountryService()
+        {
+            _countries = new List<Country>
+            {
+                new Country
+                {
+                    Id = 1,
+                    Name = "Canada"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "USA"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Mexico"
+                }
+            };
+        }
+
+        public async Task<Country> Get(int id)
+        {
+            return await Task.FromResult(_countries.FirstOrDefault(c => c.Id == id));
+        }
+
+        public async Task<List<Country>> Get()
+        {
+            return await Task.FromResult(_countries);
+        }
+    }
+}
